f16e74b9a1f47e51e5a6363efccd72e0c045e5f7
salva608 <totoalahielmejor@gmail.com>
2026-01-27 08:11:09 -0500

    borre contenido app.js

--- simulacro1/src/app.js
+++
@@ -1,7 +0,0 @@
-import { formularioHTML } from "./components/formulario.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("app").innerHTML = formularioHTML;
-});
-
-
