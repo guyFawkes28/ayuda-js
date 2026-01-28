8cf15b2e4a5d121fc9a5449bd62158f724b3f380
jpgcano <juan-0395@hotmail.com>
2026-01-28 13:01:41 -0500

    guardar codigo incompleto

--- simulacro1/src/routes/router.js
+++ simulacro1/src/routes/router.js
@@ -49,3 +49,43 @@ switch (hash) {
 }
 
 }
+
+/**
+ // routes/router.js
+import { render } from "../app.js";
+import { navbar } from "../components/navbar.js";
+import { CreateProject } from "../pages/CreateProject.js";
+import { Dashboard} from "../pages/Dashboard.js";
+import { Error } from "../pages/Error.js";
+import { Loading } from "../pages/Loading.js";
+import { getProjects } from "../services/projectsServices.js";
+
+export async function router(){
+
+    const hash = location.hash || '#/dashboard'
+
+    switch (hash) {
+        case '#/login':
+            render('<p>tedfsdfsdfds</p>')       
+            break;
+            
+        case '#/dashboard':
+        
+            const projects = await getProjects()
+            render(Dashboard(projects))
+            break;
+            
+        case '#/create-project':
+            render(CreateProject())
+            break;
+            
+        case '#/loading':
+            render(Loading())
+            break;
+            
+        default:
+            render(Error())
+            break;
+    }
+}
+ */
--- simulacro1/src/services/projectsServices.js
+++ simulacro1/src/services/projectsServices.js
@@ -7,3 +7,24 @@ export async function createProject(project) {
     body: JSON.stringify(project),
   });
 }
+
+
+/**
+ * // services/projectsServices.js
+
+const API_URL = 'http://localhost:4000/proyectos' // ⬅️ Cambiar puerto y nombre
+
+export async function createProject(project) {
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(project)
+    })
+    return await response.json()
+}
+
+export async function getProjects() {
+    const response = await fetch(API_URL)
+    return await response.json()
+}
+ */
